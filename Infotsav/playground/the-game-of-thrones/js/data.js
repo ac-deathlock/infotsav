e590655c788b67e19e7e5bc715a1fe5b179a0b1a
Arvind Rachuri <ari0997@gmail.com>
2016-08-02 21:41:08 +0530

    Added some pokemon changes

---
+++ Infotsav/playground/the-game-of-thrones/js/data.js
@@ -0,0 +1,637 @@
+var levels = [
+	{
+		id: 1,
+		characters: [
+			{
+				name: 'purple',
+				position: [1,1]
+			},
+			{
+				name: 'orange',
+				position: [3,2]
+			},
+			{
+				name: 'main',
+				position: [1,4]
+			}
+		]
+	},
+	{
+		id: 2,
+		characters: [
+			{
+				name: 'purple',
+				position: [1,2]
+			},
+			{
+				name: 'green',
+				position: [4,1]
+			},
+			{
+				name: 'yellow',
+				position: [3,3]
+			},
+			{
+				name: 'orange',
+				position: [2,0]
+			},
+			{
+				name: 'main',
+				position: [1,4]
+			}
+		]
+	},
+	{
+		id: 3,
+		characters: [
+			{
+				name: 'purple',
+				position: [3,1]
+			},
+			{
+				name: 'green',
+				position: [4,0]
+			},
+			{
+				name: 'yellow',
+				position: [2,3]
+			},
+			{
+				name: 'orange',
+				position: [0,0]
+			},
+			{
+				name: 'main',
+				position: [1,4]
+			}
+		]
+	},	
+	{
+		id: 4,
+		characters: [
+			{
+				name: 'purple',
+				position: [4,2]
+			},
+			{
+				name: 'green',
+				position: [2,2]
+			},
+			{
+				name: 'yellow',
+				position: [0,3]
+			},
+			{
+				name: 'orange',
+				position: [2,1]
+			},
+			{
+				name: 'blue',
+				position: [3,4]
+			},
+			{
+				name: 'main',
+				position: [2,0]
+			}
+		]
+	},
+	{
+		id: 5,
+		characters: [
+			{
+				name: 'purple',
+				position: [4,2]
+			},
+			{
+				name: 'green',
+				position: [1,2]
+			},
+			{
+				name: 'yellow',
+				position: [1,4]
+			},
+			{
+				name: 'orange',
+				position: [0,0]
+			},
+			{
+				name: 'main',
+				position: [2,0]
+			}
+		]
+	},	
+	{
+		id: 6,
+		characters: [
+			{
+				name: 'purple',
+				position: [4,0]
+			},
+			{
+				name: 'green',
+				position: [3,0]
+			},
+			{
+				name: 'yellow',
+				position: [0,3]
+			},
+			{
+				name: 'orange',
+				position: [0,0]
+			},
+			{
+				name: 'main',
+				position: [3,3]
+			}
+		]
+	},
+	{
+		id: 7,
+		characters: [
+			{
+				name: 'purple',
+				position: [2,0]
+			},
+			{
+				name: 'green',
+				position: [2,2]
+			},
+			{
+				name: 'yellow',
+				position: [2,3]
+			},
+			{
+				name: 'orange',
+				position: [0,1]
+			},			
+			{
+				name: 'blue',
+				position: [4,1]
+			},
+			{
+				name: 'main',
+				position: [2,1]
+			}
+		]
+	},
+	{
+		id: 8,
+		characters: [
+			{
+				name: 'purple',
+				position: [0,1]
+			},
+			{
+				name: 'green',
+				position: [2,0]
+			},
+			{
+				name: 'yellow',
+				position: [3,2]
+			},
+			{
+				name: 'orange',
+				position: [3,4]
+			},
+			{
+				name: 'main',
+				position: [1,4]
+			}
+		]
+	},
+	{
+		id: 9,
+		characters: [
+			{
+				name: 'purple',
+				position: [3,1]
+			},
+			{
+				name: 'green',
+				position: [1,2]
+			},
+			{
+				name: 'yellow',
+				position: [3,4]
+			},
+			{
+				name: 'orange',
+				position: [4,4]
+			},
+			{
+				name: 'main',
+				position: [1,4]
+			}
+		]
+	},
+	{
+		id: 10,
+		characters: [
+			{
+				name: 'purple',
+				position: [0,0]
+			},
+			{
+				name: 'green',
+				position: [2,0]
+			},
+			{
+				name: 'yellow',
+				position: [4,1]
+			},
+			{
+				name: 'orange',
+				position: [0,2]
+			},			
+			{
+				name: 'blue',
+				position: [1,4]
+			},
+			{
+				name: 'main',
+				position: [3,3]
+			}
+		]
+	},
+	{
+		id: 11,
+		characters: [
+			{
+				name: 'purple',
+				position: [2,0]
+			},
+			{
+				name: 'green',
+				position: [4,0]
+			},
+			{
+				name: 'yellow',
+				position: [0,2]
+			},
+			{
+				name: 'orange',
+				position: [1,2]
+			},			
+			{
+				name: 'blue',
+				position: [4,4]
+			},
+			{
+				name: 'main',
+				position: [1,4]
+			}
+		]
+	},
+	{
+		id: 12,
+		characters: [
+			{
+				name: 'purple',
+				position: [0,0]
+			},
+			{
+				name: 'green',
+				position: [2,0]
+			},
+			{
+				name: 'yellow',
+				position: [4,0]
+			},
+			{
+				name: 'orange',
+				position: [0,2]
+			},			
+			{
+				name: 'blue',
+				position: [0,4]
+			},
+			{
+				name: 'main',
+				position: [4,4]
+			}
+		]
+	},
+	{
+		id: 13,
+		characters: [
+			{
+				name: 'purple',
+				position: [0,0]
+			},
+			{
+				name: 'green',
+				position: [0,1]
+			},
+			{
+				name: 'yellow',
+				position: [2,0]
+			},
+			{
+				name: 'orange',
+				position: [4,1]
+			},			
+			{
+				name: 'blue',
+				position: [1,4]
+			},
+			{
+				name: 'main',
+				position: [3,3]
+			}
+		]
+	},
+	{
+		id: 14,
+		characters: [
+			{
+				name: 'purple',
+				position: [1,0]
+			},
+			{
+				name: 'green',
+				position: [2,1]
+			},
+			{
+				name: 'yellow',
+				position: [3,2]
+			},
+			{
+				name: 'orange',
+				position: [0,3]
+			},			
+			{
+				name: 'blue',
+				position: [2,4]
+			},
+			{
+				name: 'main',
+				position: [4,4]
+			}
+		]
+	},
+	{
+		id: 15,
+		characters: [
+			{
+				name: 'purple',
+				position: [0,0]
+			},
+			{
+				name: 'green',
+				position: [4,0]
+			},
+			{
+				name: 'yellow',
+				position: [2,1]
+			},
+			{
+				name: 'orange',
+				position: [0,4]
+			},			
+			{
+				name: 'blue',
+				position: [4,4]
+			},
+			{
+				name: 'main',
+				position: [2,0]
+			}
+		]
+	},
+	{
+		id: 16,
+		characters: [
+			{
+				name: 'purple',
+				position: [0,0]
+			},
+			{
+				name: 'green',
+				position: [0,2]
+			},
+			{
+				name: 'yellow',
+				position: [0,4]
+			},
+			{
+				name: 'orange',
+				position: [4,2]
+			},			
+			{
+				name: 'blue',
+				position: [4,4]
+			},
+			{
+				name: 'main',
+				position: [2,1]
+			}
+		]
+	},
+	{
+		id: 17,
+		characters: [
+			{
+				name: 'green',
+				position: [1,0]
+			},
+			{
+				name: 'yellow',
+				position: [2,0]
+			},
+			{
+				name: 'orange',
+				position: [1,4]
+			},			
+			{
+				name: 'blue',
+				position: [3,3]
+			},
+			{
+				name: 'main',
+				position: [1,1]
+			}
+		]
+	},
+	{
+		id: 18,
+		characters: [
+			{
+				name: 'green',
+				position: [0,0]
+			},
+			{
+				name: 'purple',
+				position: [2,0]
+			},
+			{
+				name: 'yellow',
+				position: [4,0]
+			},
+			{
+				name: 'orange',
+				position: [1,2]
+			},			
+			{
+				name: 'blue',
+				position: [3,3]
+			},
+			{
+				name: 'main',
+				position: [1,4]
+			}
+		]
+	},
+	{
+		id: 19,
+		characters: [
+			{
+				name: 'green',
+				position: [0,0]
+			},
+			{
+				name: 'purple',
+				position: [0,2]
+			},
+			{
+				name: 'yellow',
+				position: [4,1]
+			},
+			{
+				name: 'orange',
+				position: [3,3]
+			},
+			{
+				name: 'main',
+				position: [2,0]
+			}
+		]
+	},
+	{
+		id: 20,
+		characters: [
+			{
+				name: 'green',
+				position: [0,0]
+			},
+			{
+				name: 'purple',
+				position: [2,0]
+			},
+			{
+				name: 'yellow',
+				position: [4,0]
+			},
+			{
+				name: 'orange',
+				position: [4,3]
+			},
+			{
+				name: 'main',
+				position: [1,4]
+			}
+		]
+	}
+];
+
+var characters = {
+	purple: {
+		color: 'purple',
+		image: 'images/purple.png',
+		shadow: {
+			alpha: 0
+		}
+	},
+	blue: {
+		color: 'blue',
+		image: 'images/blue.png',
+		shadow: {
+			alpha: 0
+		}
+	},
+	green: {
+		color: 'green',
+		image: 'images/green.png',
+		shadow: {
+			alpha: 0
+		}
+	},
+	yellow: {
+		color: 'yellow',
+		image: 'images/yellow.png',
+		shadow: {
+			alpha: 0
+		}
+	},
+	orange: {
+		color: 'orange',
+		image: 'images/orange.png',
+		shadow: {
+			alpha: 0
+		}
+	},
+	main: {
+		color: '#db5705',
+		image: 'images/red.png',
+		shadow: {
+			alpha: 0
+		}
+	},
+	selected: {		
+		shadow: {
+			center: {
+				color: '#00cafd',
+				blur: 10,
+				offset: [0, 0],
+				alpha: 1
+			},
+			right: {
+				color: '#00cafd',
+				blur: 7,
+				offset: [-5, 0],
+				alpha: 1
+			},
+			left: {
+				color: '#00cafd',
+				blur: 7,
+				offset: [5, 0],
+				alpha: 1
+			},
+			top: {
+				color: '#00cafd',
+				blur: 7,
+				offset: [0, 5],
+				alpha: 1
+			},
+			bottom: {
+				color: '#00cafd',
+				blur: 7,
+				offset: [0, -5],
+				alpha: 1
+			},
+		}
+	},
+	option: {
+		color: 'rgba(16,185,200,0.3)',
+		shadow: {
+			alpha: 0
+		}
+	},
+	centerCube: {
+		image: 'images/center.png'
+	}
+};
+
+var sounds = {
+	hooray: {
+		ogg: 'audio/hooray.ogg',
+		mp3: 'audio/hooray.mp3'
+	},
+	move: {
+		ogg: 'audio/move.ogg',
+		mp3: 'audio/move.mp3'
+	},
+	main: {
+		ogg: 'audio/Game_of_Thrones_Theme.ogg',
+		mp3: 'audio/Game_of_Thrones_Theme.mp3'
+	}
+};
